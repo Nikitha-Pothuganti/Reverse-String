cf95cdc39ba9e903d3f0ee1716e155a479fa3154
Nikitha_Pothuganti <pothugantinikitha27@gmail.com>
2025-01-29 19:55:50 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);{
+            System.out.println("Enter a String: ");
+            String string=src.nextLine();
+            String reverse=new StringBuilder(string).reverse().toString();
+            System.out.println("Reversed String: "+reverse);
+            src.close();
+        }
+    }
+}
+
